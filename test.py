3eca8263c6dfa72ebed145464e213c9f011e561d
Aryakumarjaiswal <86922289+Aryakumarjaiswal@users.noreply.github.com>
2024-11-21 11:46:12 +0530

    File Added Successfuly

---
+++ test.py
@@ -0,0 +1,29 @@
+#GITHUB COMMANDS
+# 1]   
+# Initialize Git (if not already done)
+git init
+
+# Check for branches
+git branch
+
+# If no branches exist, create the default branch
+git checkout -b main
+
+# Create and switch to a new branch
+git checkout -b MyBranch
+
+# (Optional) Push the new branch to remote
+git push -u origin MyBranch
+
+
+
+#See Current branch
+git status
+
+######################################################
+1. git init
+2. git remote add origin github_repo_link
+3. git add filename1 filename2.... || git add .(To add all files)
+4. git commit -m "message"
+
+5. git push origin branch_name
